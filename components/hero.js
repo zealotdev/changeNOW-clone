3ba7d8c427ad4935c95ab57484f36926a68b845a
zealotdev <g33ky.me@pm.me>
2021-09-04 14:47:57 +0300

    feat:hero + responsiveness completed ✅

--- components/hero.js
+++ components/hero.js
@@ -2,20 +2,44 @@ import { Tabs } from './tabs';
 
 const Hero = () => {
   return (
-    <div className="hero-bg bg-cover bg-no-repeat bg-center w-full h-screen flex justify-between">
-      <div className="text-left flex flex-col items-start space-y-4  mt-20">
-        <h1 className="text-5xl font-normal tracking-wider leading-relaxed text-white">
-          Limitless Crypto <br /> Exchange
-        </h1>
-        <span className="text-gray-600 text-3xl">
-          Fast Crypto Swap, Free of Custody
-        </span>
-        <button className="py-1 px-3 text-yellow-400 border border-yellow-400 hover:text-white hover:bg-yellow-400 transition-all duration-300">
-          Get recommended Wallet
-        </button>
+    <div className="md:hero-bg bg-cover bg-no-repeat bg-center w-full h-screen ">
+      <div className="flex flex-col md:flex-row justify-between">
+        <div className="text-left flex flex-col items-start space-y-4 mt-10 md:mt-20">
+          <h1 className="text-5xl font-normal tracking-wider leading-relaxed text-white">
+            Limitless Crypto <br /> Exchange
+          </h1>
+          <span className="text-gray-600 text-3xl">
+            Fast Crypto Swap, Free of Custody
+          </span>
+          <button className="py-1 px-3 text-yellow-400 border border-yellow-400 hover:text-white hover:bg-yellow-400 transition-all duration-300">
+            Get recommended Wallet
+          </button>
+        </div>
+        <div>
+          <Tabs></Tabs>
+        </div>
       </div>
-      <div>
-        <Tabs></Tabs>
+      <div className="mt-28 flex flex-row-reverse md:flex-row  justify-evenly md:justify-between text-white items-center ">
+        <div>
+          Buy Crypto with
+          <img src="./svg/visa-master.svg" alt="" />
+        </div>
+        <div>
+          <img
+            className="hidden md:block"
+            src="./svg/trustpilot-fresh.svg"
+            alt=""
+          />
+          <img className="md:hidden" src="./svg/index/trust-pilot.svg" alt="" />
+        </div>
+      </div>
+      <div className="my-10 w-full">
+        <img
+          className="hidden mx-auto md:block transition duration-500 ease-in-out transform hover:scale-105 lg:cursor-pointer"
+          src="./svg/premium-banner.svg"
+          alt=""
+        />
+        <img className="md:hidden mx-auto" src="./svg/mobile-apps.svg" alt="" />
       </div>
     </div>
   );
--- components/tabs.js
+++ components/tabs.js
@@ -18,8 +18,8 @@ const Tabs = () => {
   };
 
   return (
-    <div className="md:tab-w-lg tab-bg rounded-lg bg-gray-700 mt-10 text-sm font-semibold">
-      <div className="flex text-white mx-auto w-full justify-evenly">
+    <div className="md:tab-w-lg tab-bg rounded-lg bg-gray-700 mt-10 text-sm font-semibold ">
+      <div className="flex text-white mx-auto w-full justify-evenly ">
         <button
           className={
             activeTab == 'tab 1'
@@ -48,27 +48,27 @@ const Tabs = () => {
           {/* </span> */}
         </button>
       </div>
-      <div className="tab-bg tab-content-border py-4 px-6 rounded-lg rounded-t-none">
+      <div className="tab-bg tab-content-border py-4 px-6 rounded-lg rounded-t-none relative">
         {activeTab == 'tab 1' && (
           <div>
-            <div className="input-field text-2xl border border-yellow-200 rounded-t-lg text-white flex">
-              <div>
+            <div className="input-field text-lg md:text-2xl border border-yellow-200 rounded-t-lg text-white flex">
+              <div className="relative">
                 <input
-                  className="pb-4 pl-6 pt-8 bg-transparent"
+                  className="pb-4 pl-4 md:pl-6 pt-8 bg-transparent"
                   type="text"
                   name="youpay"
                   id="youpay"
                   value="0.1"
                 />
                 <label
-                  className="relative -left-56 -top-8 text-gray-500 font-light text-sm"
+                  className="absolute left-2 top-2 text-gray-500 font-light text-xs md:text-sm"
                   htmlFor="youpay"
                 >
                   You Send
                 </label>
               </div>
               <select
-                className="relative -right-7 rounded-tr-lg py-6 px-12 text-2xl text-white w-44"
+                className="absolute right-7 rounded-tr-lg py-6 px-2 md:px-12 text-xl md:text-2xl text-white w-24 md:w-44"
                 name="currency"
                 id="currency"
               >
@@ -78,7 +78,7 @@ const Tabs = () => {
               </select>
             </div>
             <div className="my-4 flex justify-between rounded-lg">
-              <div className="flex flex-col text-left text-white font-light text-sm space-y-3">
+              <div className="flex flex-col text-left text-white font-light text-sm space-y-3 overflow-x-auto">
                 <span className="border-b border-dashed w-24 border-green-500">
                   No extra fees
                 </span>
@@ -86,74 +86,62 @@ const Tabs = () => {
                   Estimated rate 1BTC ~ 38345 USD
                 </span>
               </div>
-              <div className="exchange-btn flex h-7 rounded-lg">
-                <button>
-                  <svg
-                    className="stroke-current stroke-2 transform rotate-90  stroke-color__active"
-                    width="21"
-                    height="10"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M15.224 2.55l2.609 2.61-2.609 2.608M2.889 5.159h14.674"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
-                </button>
-                <button>
-                  <svg
-                    className="stroke-current stroke-2 transform -rotate-90 font-extrabold stroke-color__active"
-                    width="21"
-                    height="10"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M15.224 2.55l2.609 2.61-2.609 2.608M2.889 5.159h14.674"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
-                </button>
+              <div className="exchange-btn h-7 rounded-lg p-1">
+                <svg
+                  className="transform rotate-90"
+                  width="25"
+                  height="23"
+                  viewBox="-2 -2 25 23"
+                  id="exchange-arrows"
+                  x="262"
+                  y="162"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M18.006 4.99l-2.992 2.996-.338.338.675.676.34-.338 3.65-3.655.507-.507-.337-.338L15.69.338 15.35 0l-.674.676.338.338 2.97 2.976H1v1h17.006zm-15.166 10l2.99 2.996.34.338-.676.676-.338-.338-3.65-3.655L1 14.5l.338-.338 3.818-3.824.338-.338.675.676-.34.338-2.97 2.976h16.983v1H2.84z"
+                    stroke="#00C26F"
+                    fill="#00C26F"
+                    fill-rule="evenodd"
+                    stroke-linecap="square"
+                  />
+                </svg>
               </div>
             </div>
             <div className="input-field text-2xl rounded-t-lg text-white flex">
-              <div>
+              <div className="relative">
                 <input
-                  className="pb-4 pl-6 pt-8 bg-transparent"
+                  className="pb-4 pl-4 md:pl-6 pt-8 bg-transparent"
                   type="text"
                   name="youget"
                   id="youget"
-                  value="1500"
+                  value="-"
                 />
                 <label
-                  className="relative -left-56 -top-8 text-gray-500 font-light text-sm"
+                  className="absolute left-2 top-2 text-gray-500 font-light text-xs md:text-sm"
                   htmlFor="youget"
                 >
                   You Get
                 </label>
               </div>
               <select
-                className="relative -right-12 rounded-tr-lg py-6 px-12 text-2xl text-white w-44"
+                className="absolute right-6 rounded-tr-lg py-7 px-2 md:px-12 text-xl md:text-2xl text-white w-24 md:w-44"
                 name="currency"
                 id="currency"
               >
+                <option value="ETH">ETH</option>
                 <option value="BTC">BTC</option>
-                <option value="EUR">EUR</option>
-                <option value="GBP">GBP</option>
+                <option value="USDT">USDT</option>
               </select>
             </div>
-            <button className="bg-green-500 w-full mt-10 mb-4 py-3 rounded-lg text-white font-bold text-lg">
+            <button className="bg-green-500 w-full mt-10 mb-4 py-3 rounded-lg text-white font-semibold md:font-bold text-base md:text-lg">
               Exchange
             </button>
           </div>
         )}
         {activeTab == 'tab 2' && (
           <div>
-            <div className="input-field text-2xl border border-yellow-200 rounded-t-lg text-white flex">
-              <div>
+            <div className="input-field text-lg md:text-2xl border border-yellow-200 rounded-t-lg text-white flex">
+              <div className="relative">
                 <input
                   className="pb-4 pl-6 pt-8 bg-transparent"
                   type="text"
@@ -162,14 +150,14 @@ const Tabs = () => {
                   value="1500"
                 />
                 <label
-                  className="relative -left-56 -top-8 text-gray-500 font-light text-sm"
+                  className="absolute left-2 top-2 text-gray-500 font-light text-xs md:text-sm"
                   htmlFor="youpay"
                 >
                   You Pay
                 </label>
               </div>
               <select
-                className="relative -right-10 rounded-tr-lg py-6 px-12 text-2xl text-white w-44"
+                className="absolute right-7 rounded-tr-lg py-6 px-2 md:px-12 text-xl md:text-2xl text-white w-24 md:w-44"
                 name="currency"
                 id="currency"
               >
@@ -245,7 +233,7 @@ const Tabs = () => {
               </div>
             </div>
             <div className="input-field text-2xl rounded-t-lg text-white flex">
-              <div>
+              <div className="relative">
                 <input
                   className="pb-4 pl-6 pt-8 bg-transparent"
                   type="text"
@@ -254,14 +242,14 @@ const Tabs = () => {
                   value="1500"
                 />
                 <label
-                  className="relative -left-56 -top-8 text-gray-500 font-light text-sm"
+                  className="absolute left-2 top-2 text-gray-500 font-light text-xs md:text-sm"
                   htmlFor="youget"
                 >
                   You Get
                 </label>
               </div>
               <select
-                className="relative -right-12 rounded-tr-lg py-6 px-12 text-2xl text-white w-44"
+                className="absolute right-6 rounded-tr-lg py-7 px-2 md:px-12 text-xl md:text-2xl text-white w-24 md:w-44"
                 name="currency"
                 id="currency"
               >
@@ -270,7 +258,7 @@ const Tabs = () => {
                 <option value="GBP">GBP</option>
               </select>
             </div>
-            <button className="bg-green-500 w-full mt-10 mb-4 py-3 rounded-lg text-white font-bold text-lg">
+            <button className="bg-green-500 w-full mt-10 mb-4 py-3 rounded-lg text-white font-semibold md:font-bold text-base md:text-lg">
               Sell
             </button>
           </div>
